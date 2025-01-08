359b20282368c588737a8b9f83a6f7359ffbb19b
soyunhan <sooho15@nate.com>
2025-01-08 10:32:10 +0900

    feat: 로그인 페이지 세팅하기

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+  import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "pages/home";
 import PostList from "pages/posts";
 import PostDetail from "pages/posts/detail";
@@ -7,20 +7,34 @@ import PostEdit from "pages/posts/edit";
 import ProfilePage from "pages/profile";
 import LoginPage from "pages/login";
 import SignupPage from "pages/signup";
+import { useState } from "react";
 
 export default function Router() {
+  // firebase Auth가 인증되었으면 true로 변경해주는 로직 추가
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route path="/posts/new" element={<PostNew />} />
-        <Route path="/posts/edit/:id" element={<PostEdit />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isAuthenticated ? (
+          <>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<PostList />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
+          <Route path="/posts/new" element={<PostNew />} />
+          <Route path="/posts/edit/:id" element={<PostEdit />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<LoginPage />} />
+          </>
+        )};
+        
       </Routes>
     </>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,9 @@
+import Header from "components/Header";
+import LoginForm from "components/LoginForm";
+
 export default function LoginPage() {
-    return <h2>Login</h2>;
+    return (<>
+    <Header/>
+    <LoginForm/>
+    </>);
   }
